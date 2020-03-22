601d9d483d313848effdbada48b1cde8e2696f6a
Flann Corcoran <flann@posteo.net>
2020-03-22 12:49:48 +0100

    fiexed print_last_local_record so it *actually* *prints* the *last* record!!!

--- pi_logger/print_last_local_record.py
+++ pi_logger/print_last_local_record.py
@@ -8,12 +8,13 @@ def print_last_local_record():
     log_path = os.path.join(os.path.expanduser("~"), "logs")
     db_path = os.path.join(log_path, "locallogs.db")
     conn_string = 'sqlite:///{}'.format(db_path)
-    engine = create_engine(conn_string, echo=False)
+    engine = create_engine(conn_string, echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(LocalData).first()
-    print(result)
+    result = session.query(LocalData).slice(-2, -1).first()
+    return result
 
 
 if __name__ == "__main__":
-    print_last_local_record()
+    result = print_last_local_record()
+    print(result)
