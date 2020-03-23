f2d7c0f626b48a7453a7126aaa06891b4f7f3588
flann <flann@posteo.net>
2020-03-23 16:05:24 +0100

    split set_up script in two to allow service section to be run as root. added flag files to .gitignore

---
+++ set_up_db.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# check to see if this has been done already
+if [ ! -e local_db_set_up_complete ]
+then
+  # create database and import existing records
+  echo "running local_db.py"
+  python3 pi_logger/local_db.py
+  echo "running import_existing.py"
+  python3 pi_logger/import_existing.py
+  
+  echo "creating file 'local_db_set_up_complete' as flag"
+  touch local_db_set_up_complete
+fi
--- set_up_service.sh
+++ set_up_service.sh
@@ -1,14 +1,8 @@
 #! /bin/bash
 
 # check to see if this has been done already
-if [ ! -e local_db_set_up_complete ]
+if [ ! -e logger_service_set_up_complete ]
 then
-  # create database and import existing records
-  echo "running local_db.py"
-  python3 pi_logger/local_db.py
-  echo "running import_existing.py"
-  python3 pi_logger/import_existing.py
-
   # set up logging as a service
   echo "copying logger.service to  /etc/systemd/system/"
   cp logger.service /etc/systemd/system/logger.service
@@ -16,7 +10,6 @@ then
   systemctl daemon-reload
   systemctl start logger.service
   systemctl enable logger.service
-
-  echo "creating file 'local_db_set_up_complete' as flag"
-  touch local_db_set_up_complete
+  echo "creating file 'logger_service_set_up_complete as flag"
+  touch logger_service_set_up_complete
 fi
