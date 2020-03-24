0bf334849fe0d65c333831cb752c58fb382813b2
Flann Corcoran <flann@posteo.net>
2020-03-24 09:29:12 +0100

    Turned off debug mode

--- pi_logger/api_server.py
+++ pi_logger/api_server.py
@@ -67,4 +67,4 @@ api.add_resource(PollSensors, '/poll_sensors')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5002', debug=True)
+    app.run(host='0.0.0.0', port='5002', debug=False)
